3a76fd0f80e12f0f86193bed9e0a3eef07b72a36
minoee <miooi@inha.edu>
2024-02-08 14:09:36 +0900

    0208

--- 1.cpp
+++ 1.cpp
@@ -1,43 +1,25 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
+int main() {
+    int user, ans, cnt = 0;
+    srand(time(0));
+    ans = rand() % 100 + 1;
 
-int input();
-bool process(int year);
-void output(int year, bool result);
-
-int main(){
-    int year = input();
-    bool result = process(year);
-    output(year, result);
-
-}
-
-int input(){
-    int year;
-    do{
-        cout << "Input year : ";
-        cin >> year;
-    }while(year <= 1582);
-    return year;
-}
-
-bool process(int year){
-    bool criteria1 = (year % 4 == 0);
-    bool criteria2 = (year % 100 != 0) || (year % 400 == 0);
-    return (criteria1) && (criteria2);
-}
-
-void output(int year, bool result){
-    if (result){
-        cout << year << " is leap year";
+    while (true) {
+        cin >> user;
+        cnt++;
+        if (user == ans) {
+            cout << "find" << '\n' << cnt;
+            break;
+        }
+        else if (user < ans) {
+            cout << "lower\n";
+        }
+        else {
+            cout << "bigger\n";
+        }
     }
-    else{
-        cout << year << " is not leap year";
-    }
-    return;
 }
-
-
-
-
